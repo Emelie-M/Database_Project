f2840ff817f7b0b2156253f2d19b7b39fcd30b18
Emelie-M <emelie.mejia21@gmail.com>
2025-10-24 14:14:18 -0400

    Organized and added the layout for the Inserts

--- Project.sql
+++ Project.sql
@@ -1,30 +1,10 @@
-
+--Tables
 CREATE TABLE Student (
     StudentID INT  IDENTITY(1,1) PRIMARY KEY,
     FirstName VARCHAR(30) NOT NULL,
     LastName VARCHAR(30) NOT NULL,
     Email VARCHAR(100) UNIQUE NOT NULL,
     Year VARCHAR(10),
-);
-
-CREATE TABLE Registration (
-    RegistrationID INT  IDENTITY(1,1) PRIMARY KEY,
-    Status VARCHAR(15),
-    TimeStamp date,
-	StudentID int,
-	EventID int,
-	FOREIGN KEY (StudentID) REFERENCES Student(StudentID),
-	FOREIGN KEY (EventID) REFERENCES Event(EventID)
-);
-
-CREATE TABLE Event (
-    EventID INT  IDENTITY(1,1) PRIMARY KEY,
-    Title VARCHAR(30) NOT NULL,
-    Date date,
-    Location VARCHAR(100) NOT NULL,
-    Capacity int CHECK (Capacity < 300),
-	OrganizerID int,
-	FOREIGN KEY (OrganizerID) REFERENCES Organizer(OrganizerID)
 );
 
 CREATE TABLE Organizer (
@@ -34,3 +14,38 @@ CREATE TABLE Organizer (
     Email VARCHAR(100) UNIQUE NOT NULL,
     Phone VARCHAR(15),
 );
+
+CREATE TABLE Event (
+    EventID INT  IDENTITY(1,1) PRIMARY KEY,
+	OrganizerID int,
+    Title VARCHAR(30) NOT NULL,
+    Date date,
+    Location VARCHAR(100) NOT NULL,
+    Capacity int CHECK (Capacity < 300),
+	FOREIGN KEY (OrganizerID) REFERENCES Organizer(OrganizerID)
+);
+
+CREATE TABLE Registration (
+    RegistrationID INT  IDENTITY(1,1) PRIMARY KEY,
+	StudentID int,
+	EventID int,
+    Status VARCHAR(15),
+    TimeStamp date,
+	FOREIGN KEY (StudentID) REFERENCES Student(StudentID),
+	FOREIGN KEY (EventID) REFERENCES Event(EventID)
+);
+
+
+
+--Values
+INSERT INTO Student (FirstName, LastName, Email, Year) VALUES
+('Kusuo','Saiki','saiki.k@gmail.com','Year 1');
+
+INSERT INTO Organizer (FirstName, LastName, Email, Phone) VALUES
+('','','','');
+
+INSERT INTO Event (Title, Date, Location, Capacity) VALUES
+('','','','');
+
+INSERT INTO Registration (Status, TimeStamp) VALUES
+('','');
